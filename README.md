0c5823f3d9b89cd2a1aae7cad4302a15dc398b3b
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-09-20 23:21:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CHAMBRA MOBILE :coffee:
+
+Chat in real time based in my project named Chambra with Expo.
+
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0-blue.svg?cacheSeconds=2592000" />
+</p>
---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ["babel-preset-expo"],
+		plugins: [
+			"react-native-paper/babel",
+			[
+				"module-resolver",
+				{
+					root: ["./src"],
+					alias: {
+						_shared: "./src/shared",
+					},
+				},
+			],
+		],
+	};
+};
